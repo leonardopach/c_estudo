64326e547af5035300609e763a1093a9835d1b8f
leonardopach <pachecodossantos17@gmail.com>
2023-10-02 18:53:00 -0300

    Operations on Data

--- OperationsOnData/logicolOperator.cpp
+++ OperationsOnData/logicolOperator.cpp
@@ -11,15 +11,21 @@ int main()
     std::cout << "Basic AND operations" << std::endl;
 
     std::cout << "a && b : " << (a && b) << std::endl;
-    std::cout << "a && b : " << (a && c) << std::endl;
+    std::cout << "a && c : " << (a && c) << std::endl;
     std::cout << "a && b && c : " << (a && b && c) << std::endl;
 
     std::cout << std::endl;
     std::cout << "Basic OR operations" << std::endl;
 
     std::cout << "a || b : " << (a || b) << std::endl;
-    std::cout << "a || b : " << (a || c) << std::endl;
+    std::cout << "a || c : " << (a || c) << std::endl;
     std::cout << "a || b || c : " << (a || b || c) << std::endl;
+
+    std::cout << "Basic NOT operations" << std::endl;
+
+    std::cout << "!a : " << !a << std::endl;
+    std::cout << "!b : " << !b << std::endl;
+    std::cout << "!c : " << !c << std::endl;
 
     return 0;
 }
