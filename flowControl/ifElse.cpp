ba4f66755aca6ffb3455ed45ed339e91ee0487ad
leonardo <pachecodossantos17@gmail.com>
2023-10-04 10:42:12 -0300

    flow control

--- flowControl/ifElse.cpp
+++ flowControl/ifElse.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
-const int Pen{10};
-const int Marker{20};
-const int Eraser{30};
-const int Rectangle{40};
-const int Circle{50};
-const int Ellipse{60};
+const int Pen{ 10 };
+const int Marker{ 20 };
+const int Eraser{ 30 };
+const int Rectangle{ 40 };
+const int Circle{ 50 };
+const int Ellipse{ 60 };
 
 int main() {
 
-  int tool{Eraser};
+  int tool{ Eraser };
 
   if (tool == Pen) {
     std::cout << "Active tool is pen" << std::endl;
